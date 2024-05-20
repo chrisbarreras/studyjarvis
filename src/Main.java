745f4e96afa280ea5b66f9a18a4b8ed1e2f77a5a
tombarreras <tombarreras@yahoo.com>
2024-05-19 17:41:42 -0700

    uncomment

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    Gemini g1 = new Gemini("studyjarvis", "us-west1");
 
-    Scanner scanner = new Scanner(System.in);
-//    System.out.print("Enter a prompt: ");
-//    String prompt = scanner.nextLine();
-//
-//    System.out.println(g1.textInput(prompt));
+    public static void main(String[] args) throws IOException {
+        Gemini g1 = new Gemini("studyjarvis", "us-west1");
+
+        Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a prompt: ");
+    String prompt = scanner.nextLine();
+
+    System.out.println(g1.textInput(prompt));
+    }
 }
