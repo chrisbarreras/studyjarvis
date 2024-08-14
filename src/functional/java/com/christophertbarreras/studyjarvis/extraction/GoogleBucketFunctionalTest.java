24a0f3553dafdd49fe5dabddbab045d8168abcbb
chrisbarreras <chris.barreras@yahoo.com>
2024-08-13 19:29:10 -0700

    added GoogleBucketFunctionalTest

--- src/functional/java/com/christophertbarreras/studyjarvis/extraction/GoogleBucketFunctionalTest.java
+++ src/functional/java/com/christophertbarreras/studyjarvis/extraction/GoogleBucketFunctionalTest.java
@@ -4,14 +4,26 @@ import com.christophertbarrerasconsulting.studyjarvis.GoogleBucket;
 import com.christophertbarrerasconsulting.studyjarvis.command.CommandSession;
 import com.christophertbarrerasconsulting.studyjarvis.file.AppSettings;
 import com.christophertbarrerasconsulting.studyjarvis.file.ConfigReader;
+import org.junit.jupiter.api.Assertions;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Properties;
 
 public class GoogleBucketFunctionalTest {
     @org.junit.jupiter.api.Test
     void clearBucketClearsBucket () throws IOException {
         Properties properties = ConfigReader.readProperties();
+
+        int numberOfItemsInBucketFirstCount = GoogleBucket.getInstance(properties.getProperty(AppSettings.BucketName.toString())).countBucket();
+        GoogleBucket.getInstance(properties.getProperty(AppSettings.BucketName.toString())).uploadDirectoryContents(Path.of("src/functional/resources/files"));
+        int numberOfItemsInBucketSecondCount = GoogleBucket.getInstance(properties.getProperty(AppSettings.BucketName.toString())).countBucket();
+
+        Assertions.assertNotEquals(numberOfItemsInBucketFirstCount, numberOfItemsInBucketSecondCount);
+
         GoogleBucket.getInstance(properties.getProperty(AppSettings.BucketName.toString())).clearBucket();
+        int numberOfItemsInBucketThirdCount = GoogleBucket.getInstance(properties.getProperty(AppSettings.BucketName.toString())).countBucket();
+
+        Assertions.assertNotEquals(numberOfItemsInBucketSecondCount, numberOfItemsInBucketThirdCount);
     }
 }
