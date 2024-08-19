117fde29f998817217977d738e938544c2f8fd5e
tombarreras <tombarreras@yahoo.com>
2024-08-19 15:46:51 -0700

    Better SQLs for creating tables

--- src/main/sql/5-create-tables.sql
+++ src/main/sql/5-create-tables.sql
@@ -1,7 +1,8 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL,
+    is_administrator BOOLEAN NOT NULL DEFAULT FALSE
 );
 CREATE TABLE sessions (
     session_id SERIAL PRIMARY KEY,
