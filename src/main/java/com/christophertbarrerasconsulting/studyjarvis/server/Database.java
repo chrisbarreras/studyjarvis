09a6b12b663bbf8d7d99db862d4c86317c75c976
chrisbarreras <chris.barreras@yahoo.com>
2024-08-20 15:26:24 -0700

    added dependencies

--- src/main/java/com/christophertbarrerasconsulting/studyjarvis/server/Database.java
+++ src/main/java/com/christophertbarrerasconsulting/studyjarvis/server/Database.java
@@ -1,12 +1,21 @@
 package com.christophertbarrerasconsulting.studyjarvis.server;
+
 import java.sql.*;
 
 class Database {
-    private static final String URL = System.getenv("STUDYJARVIS_DB_URL") ;
+    private static final String URL = System.getenv("STUDYJARVIS_DB_URL");
     private static final String USER = System.getenv("STUDYJARVIS_DB_USER");
     private static final String PASS = System.getenv("STUDYJARVIS_DB_PASSWORD");
 
     public static Connection connect() throws SQLException {
+        try {
+            // Load the PostgreSQL driver
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL Driver not found!", e);
+        }
+
+        // Establish the connection
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
